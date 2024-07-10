7dcc915cd5e9b9d80be56132d56c7d2af9e6eeab
guido <punkedwerewolf@protonmail.com>
2024-07-10 03:29:25 -0700

    d

--- public/js/lyt.js
+++ public/js/lyt.js
@@ -1,1 +1,1 @@
-$(function(){$("#hed").load("/lyt/hed.html");$("#nvw").load("/lyt/nav.html");$("#rgt").load("/lyt/rgt.html");$("#fot").load("/lyt/fot.html");});
+$(function(){$("#hed").load("/lyt/hed.html");$("#nvw").load("/lyt/nav.html");$("#rgt").load("/lyt/rgt.html");$("#hed").load("/lyt/hed.html");$("#fot").load("/lyt/fot.html");});
